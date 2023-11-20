f66d05f23aff0dc672482af920d49c6a600e1df0
David Huang <davidhuang@wifi-10-41-133-68.wifi.berkeley.edu>
2023-11-20 14:38:07 -0800

    update

--- src/time_conversion.py
+++ src/time_conversion.py
@@ -7,11 +7,9 @@ class TimeFormatConverter:
         self.predictions_model = predictions_model
 
     def convert_time_format_for_intervals(self, lead_time=None, variable=None):
-        print("debug start")
         datetime_values = pd.date_range(start='2020-01-01', end='2020-12-31T18:00:00', freq='6H')
         one_year_in_ns = np.timedelta64(int(365 * 24 * 60 * 60 * 1e9), 'ns')
         lead_time = np.timedelta64(int(lead_time * 365 * 24 * 60 * 60 * 1e9), 'ns') if lead_time else one_year_in_ns
-        print("debug: 1")
         print(self.predictions_model)
         new_dataset = xr.Dataset(
             coords={
@@ -22,29 +20,28 @@ class TimeFormatConverter:
                 'latitude': self.predictions_model['latitude']
             }
         )
-        print("debug: 2")
 
         new_dataset["geopotential_upper"] = xr.DataArray(np.nan, dims=("time", "level", "longitude", "latitude"), coords=new_dataset.coords)
         new_dataset["geopotential_lower"] = xr.DataArray(np.nan, dims=("time", "level", "longitude", "latitude"), coords=new_dataset.coords)
-
+        
+        tracker = 0 
         for date in datetime_values:
+            print(tracker)
+            tracker += 1
+
             day_of_year = date.dayofyear
+            # TODO: This might be a faster operation: new_dataset["geopotential_upper"].loc[date] = self.predictions_model["geopotential_upper"].loc[{'time': day_of_year}]
             new_dataset["geopotential_upper"].loc[date] = self.predictions_model["geopotential_upper"].sel(time=day_of_year, method="nearest")
             new_dataset["geopotential_lower"].loc[date] = self.predictions_model["geopotential_lower"].sel(time=day_of_year, method="nearest")
 
-        print("debug: 3")
         new_dataset = new_dataset.expand_dims(prediction_timedelta=[lead_time])
         self._update_dataset_attributes(new_dataset)
-        print("debug: 4")
         return new_dataset
 
     def convert_time_format(self, lead_time=None, variable=None):
-        print("debug start for convert_time_format")
-        print(f"variable: {variable}")
         datetime_values = pd.date_range(start='2020-01-01', end='2020-12-31T18:00:00', freq='6H')
         one_year_in_ns = np.timedelta64(int(365 * 24 * 60 * 60 * 1e9), 'ns')
         lead_time = np.timedelta64(int(lead_time * 365 * 24 * 60 * 60 * 1e9), 'ns') if lead_time else one_year_in_ns
-        print("debug: 1")
 
         new_dataset = xr.Dataset(
             coords={
@@ -55,19 +52,15 @@ class TimeFormatConverter:
                 'latitude': self.predictions_model['latitude']
             }
         )
-        print("debug: 2")
 
         new_dataset[variable] = xr.DataArray(dims=("time", "level", "longitude", "latitude"), coords=new_dataset.coords)
-        print("debug: 3")
 
         for date in datetime_values:
             day_of_year = pd.to_datetime(date).dayofyear
             new_dataset[variable].loc[{'time': date}] = self.predictions_model[variable].loc[{'time': day_of_year}].values
 
-        print("debug: 4")
         new_dataset = new_dataset.expand_dims(prediction_timedelta=[lead_time])
         self._update_dataset_attributes(new_dataset, variable)
-        print("debug: 5")
         self.predictions_model = new_dataset
         return self.predictions_model
 
