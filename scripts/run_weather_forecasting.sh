d892e1e9c58bc753d6a20748d1a947820265fe0a
David Huang <davidhuang@Davids-MacBook-Pro.local>
2023-11-20 12:50:54 -0800

    script update

--- scripts/run_weather_forecasting.sh
+++ scripts/run_weather_forecasting.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # Set GPU ID
-GPU=0
+#GPU=0
 
 # Activate Conda environment (if needed)
 source activate weather-research
 
 # Set other parameters if needed
 OBS_PATH='gs://weatherbench2/datasets/era5/1959-2022-6h-64x32_equiangular_with_poles_conservative.zarr'
-START_YEAR=2010
-END_YEAR=2020
+START_YEAR=2015
+END_YEAR=2019
 WEATHER_VARIABLE="geopotential"
 PROBABILISTIC=true
 CONFIDENCE_INTERVALS=true
 
 # Set CUDA device
-export CUDA_VISIBLE_DEVICES=$GPU
+#export CUDA_VISIBLE_DEVICES=$GPU
 
 # Run the Python script
-python -u main.py \
+python -u "/Users/davidhuang/Desktop/Weather-Research /Probabilistic-Weather-Forecasting-/src/main.py" \
     --obs_path "$OBS_PATH" \
     --start_year $START_YEAR \
     --end_year $END_YEAR \
